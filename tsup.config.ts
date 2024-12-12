681161561b622850351aef7b94fb9123e0c4e31e
fulcanelly <dallasxfulcanelly@gmail.com>
2024-12-12 03:25:21 +0200

    fut: adjust tsup to generate schema js as well

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: [
+    "src/index.ts",
+    "src/schema.ts",
+  ],
   format: ["cjs", "esm"],
   dts: true,
   splitting: false,
